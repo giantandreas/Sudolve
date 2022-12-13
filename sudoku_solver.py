a9d40e3e2e88d75fb74d9f5ab23551e5398b3e32
Giant Andreas Tambunan <68510068+giantandreas@users.noreply.github.com>
2022-12-13 15:29:59 +0700

    add sudoku solver script

---
+++ sudoku_solver.py
@@ -0,0 +1,15 @@
+from sudoku import Sudoku
+import numpy as np
+
+
+# read unsolved board from txt file
+board = np.loadtxt("sudoku.txt", dtype='i', delimiter=',')
+
+# solve the sudoku
+puzzle = Sudoku(3, 3, board=board.tolist())
+solution = puzzle.solve()
+
+# write the solution back to txt file
+f = open('sudoku.txt', 'w')
+np.savetxt(f, solution.board, fmt='%i', delimiter=',', newline='\n')
+
