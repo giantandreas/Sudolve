77252b35df808a0a954cb3ccbdc7e4080f8ba814
Giant Andreas Tambunan <68510068+giantandreas@users.noreply.github.com>
2022-12-13 14:18:17 +0700

    change return type to int

--- read_number.m
+++ read_number.m
@@ -10,27 +10,26 @@ function number=read_number(char_img)
         cors = [cors cor];
     end
     index = find(cors == max(cors), 1);
-    disp(index);
-    
+
     if index == 1
-        number='1';
+        number = 1;
     elseif index == 2
-        number='2';
+        number = 2;
     elseif index == 3
-        number='3';
+        number = 3;
     elseif index == 4 || index == 5
-        number='4';
+        number = 4;
     elseif index == 6
-        number='5';
+        number = 5;
     elseif index == 7 || index == 8 || index == 9
-        number='6';
+        number = 6;
     elseif index == 10
-        number='7';
+        number = 7;
     elseif index == 11 || index == 12
-        number='8';
+        number = 8;
     elseif index == 13 || index == 14 || index == 15
-        number='9';
+        number = 9;
     else
-        number='0';
+        number = 0;
     end
 end
